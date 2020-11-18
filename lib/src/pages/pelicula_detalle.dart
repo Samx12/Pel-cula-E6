ba0b38b752f1ca04545c57536c73eeb046412e2f
Samuel Serrano Santos <al221911197@gmail.com>
2020-11-17 20:52:21 -0600

    Pgina d actores

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:pelicula/src/models/actores_model.dart';
 import 'package:pelicula/src/models/pelicula_model.dart';
+
+import '../providers/peliculas_provider.dart';
 
 class PeliculaDetalle extends StatelessWidget {
   final Pelicula pelicula;
@@ -19,6 +22,11 @@ class PeliculaDetalle extends StatelessWidget {
             SizedBox(height: 10.0),
             _posterTitulo(context, pelicula),
             _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _crearCasting(pelicula),
+
           ]),
         )
       ],
@@ -94,7 +102,61 @@ class PeliculaDetalle extends StatelessWidget {
       );
     }
 
+  Widget _crearCasting(Pelicula pelicula){
+    final peliProvider = new PeliculasProvider();
 
+    return FutureBuilder(
+      future: peliProvider.getCast(pelicula.id.toString()),
+      builder: (context, AsyncSnapshot<List> snapshot) {
+        
+        if( snapshot.hasData ) {
+          return _crearActoresPageView( snapshot.data );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+
+      },
+    );
+  }
+
+  Widget _crearActoresPageView( List<Actor> actores ) {
+
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: PageController(
+          viewportFraction: 0.3,
+          initialPage: 1
+        ),
+        itemCount: actores.length,
+        itemBuilder: (context, i) =>_actorTarjeta( actores[i] ),
+      ),
+    );
+
+  }
+
+  Widget _actorTarjeta( Actor actor ) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              image: NetworkImage( actor.getFoto() ),
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            actor.name,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      )
+    );
+  }
 
 }
 
